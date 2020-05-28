d751458a5643605c3aa1e198a52c93c2020a349a
munizdiogo <diogomuniz.web@gmail.com>
2020-05-28 12:53:18 -0300

    216- Usando Consumer

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -4,12 +4,11 @@ import '../models/products.dart';
 import '../utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
-
   // ProductItem(this.product);
 
   @override
   Widget build(BuildContext context) {
-  final Product product = Provider.of<Product>(context);
+    final Product product = Provider.of<Product>(context, listen: false);
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
@@ -28,12 +27,15 @@ class ProductItem extends StatelessWidget {
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: Icon(product.isFavorite ? Icons.favorite : Icons.favorite_border),
-            onPressed: () {
-              product.toggleFavorite();
-            },
-            color: Theme.of(context).accentColor,
+          leading: Consumer<Product>(
+            builder: (ctx, product, _) => IconButton(
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
+              onPressed: () {
+                product.toggleFavorite();
+              },
+              color: Theme.of(context).accentColor,
+            ),
           ),
           title: Text(
             product.title,
