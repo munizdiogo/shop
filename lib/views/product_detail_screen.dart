00d56eb427fbc46b4a2ebe973c4d2c1c7a3669cc
munizdiogo <diogomuniz.web@gmail.com>
2020-05-28 19:38:10 -0300

    225- Componente Detalhe do Produto.

--- lib/views/product_detail_screen.dart
+++ lib/views/product_detail_screen.dart
@@ -3,20 +3,45 @@ import 'package:flutter/material.dart';
 import '../providers/product.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-
-
-
   @override
   Widget build(BuildContext context) {
-  
-  final Product product = ModalRoute.of(context).settings.arguments as Product;
+    final Product product =
+        ModalRoute.of(context).settings.arguments as Product;
 
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
       ),
-      body: Column(
-        children: [],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'R\$ ${product.price}',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                product.description,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
