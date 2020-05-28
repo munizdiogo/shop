ddec04d334436fd1ee2ce444e2a2f8f344d2451d
munizdiogo <diogomuniz.web@gmail.com>
2020-05-28 16:38:04 -0300

    218- Carrinho e Item do Carrinho.

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,7 +1,7 @@
 
 import 'package:flutter/cupertino.dart';
-import 'package:shop/data/dummy_data.dart';
-import 'package:shop/models/products.dart';
+import '../data/dummy_data.dart';
+import 'product.dart';
 
 class Products with ChangeNotifier {
 
--- lib/views/product_detail_screen.dart
+++ lib/views/product_detail_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../models/products.dart';
+import '../providers/product.dart';
 
 class ProductDetailScreen extends StatelessWidget {
 
