5c379b576ed0d689c5847da84698d898177d3a7a
munizdiogo <diogomuniz.web@gmail.com>
2020-05-28 20:30:07 -0300

    230.2- Componente Pedidos

--- lib/views/orders_screen.dart
+++ lib/views/orders_screen.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:shop/widgets/app_drawer.dart';
+import 'package:shop/widgets/order_widget.dart';
+import '../providers/orders.dart';
+import '../widgets/app_drawer.dart';
+import 'package:provider/provider.dart';
 
 class OrdersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+  final Orders orders = Provider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Meus Pedidos'),
       ),
       drawer: AppDrawer(),
+      body: ListView.builder(
+        itemCount: orders.itemsCount,
+        itemBuilder: (ctx, i) => OrderWidget(orders.items[i]),
+      ),
     );
   }
 }
---
+++ lib/widgets/order_widget.dart
@@ -0,0 +1,29 @@
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../providers/orders.dart';
+
+class OrderWidget extends StatelessWidget {
+
+  final Order order;
+
+  const OrderWidget(this.order);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(10),
+      child: ListTile(
+        title: Text('R\$ ${order.total.toStringAsFixed(2)}'),
+        subtitle: Text(
+          DateFormat('dd/MM/yyyy hh:mm').format(order.date),
+        ),
+        trailing: IconButton(
+          icon: Icon(Icons.expand_more),
+          onPressed: (){},
+        ),
+      ),
+    );
+  }
+}
