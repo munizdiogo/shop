f81c98ad87e850441a4a5de98ac008ae1b1bd313
munizdiogo <diogomuniz.web@gmail.com>
2020-05-31 01:18:05 -0300

    279- FutureBuilder

--- lib/views/orders_screen.dart
+++ lib/views/orders_screen.dart
@@ -4,38 +4,34 @@ import '../providers/orders.dart';
 import '../widgets/app_drawer.dart';
 import 'package:provider/provider.dart';
 
-class OrdersScreen extends StatefulWidget {
-  @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    Provider.of<Orders>(context, listen: false).loadOrders().then((_) {
-      setState(() {
-        _isLoading = false;
-      });
-    });
-  }
+class OrdersScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final Orders orders = Provider.of(context);
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Meus Pedidos'),
       ),
       drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: orders.itemsCount,
-              itemBuilder: (ctx, i) => OrderWidget(orders.items[i]),
-            ),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).loadOrders(),
+        builder: (ctx, snaphop) {
+          if (snaphop.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if(snaphop.error != null) {
+            return Center(child: Text('Ocorreu um erro inesperado.'));
+          } else {
+            return Consumer<Orders>(
+              builder: (ctx, orders, child) {
+                return ListView.builder(
+                  itemCount: orders.itemsCount,
+                  itemBuilder: (ctx, i) => OrderWidget(orders.items[i]),
+                );
+              },
+            );
+          }
+        },
+      ),
     );
   }
 }
