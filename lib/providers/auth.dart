448e01b1aa25c48699fd7e5f7e31a285c6c5a259
munizdiogo <diogomuniz.web@gmail.com>
2020-06-05 11:32:55 -0300

    291- Login de Usuário

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -7,17 +7,26 @@ class Auth with ChangeNotifier {
   static const _url =
       'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCxDrttaDuSv2hsHA-xZoYwboi5pM5k8do';
 
-  Future<void> signup(String email, String password) async {
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyCxDrttaDuSv2hsHA-xZoYwboi5pM5k8do';
+
     final response = await http.post(
-      _url,
-      body: json.encode({
-        'email': email, 
-        'password': password,
-        'returnSecureToken': true
-      }),
+      url,
+      body: json.encode(
+          {'email': email, 'password': password, 'returnSecureToken': true}),
     );
 
     print(json.decode(response.body));
     return Future.value();
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
